ef1528767db13f67d884b6ec422eae486458efbd
depnicol <depnicol@student.ethz.ch>
2015-09-03 11:16:53 +0200

    minor

--- src/driverDUOstereo.cpp
+++ src/driverDUOstereo.cpp
@@ -19,21 +19,21 @@ const std::string DUOStereoDriver::CameraNames[TWO_CAMERAS] = {"left","right"};
 
 
 DUOStereoDriver::DUOStereoDriver(void):
-			 _useDUO_Imu(false),
-			 _useDUO_LEDs(false),
-			 _priv_nh("~"),
-			 _camera_nh("duo3d_camera"),
-			 _camera_name("duo_camera"),
-			 _it(new image_transport::ImageTransport(_camera_nh)),
-			 msgs_in_queue(0)
+					 _useDUO_Imu(false),
+					 _useDUO_LEDs(false),
+					 _priv_nh("~"),
+					 _camera_nh("duo3d_camera"),
+					 _camera_name("duo_camera"),
+					 _it(new image_transport::ImageTransport(_camera_nh)),
+					 msgs_in_queue(0)
 {
 	for(int i = 0; i < TWO_CAMERAS; i++)
 	{
 		_single_camera_nh[i] 	= ros::NodeHandle(_camera_nh, CameraNames[i]);  // for i-th CameraInfoManager
-      	_cinfo[i] 				= boost::shared_ptr<camera_info_manager::CameraInfoManager>
-      								(new camera_info_manager::CameraInfoManager(_single_camera_nh[i]));
-      	_calibrationMatches[i] 	= true;
-      	_imagePub[i] 			= _it->advertiseCamera(CameraNames[i]+"/image_raw", 1);
+		_cinfo[i] 				= boost::shared_ptr<camera_info_manager::CameraInfoManager>
+		(new camera_info_manager::CameraInfoManager(_single_camera_nh[i]));
+		_calibrationMatches[i] 	= true;
+		_imagePub[i] 			= _it->advertiseCamera(CameraNames[i]+"/image_raw", 1);
 	}
 
 	_pub = _camera_nh.advertise<sensor_msgs::Imu>("cam_imu", 5);
@@ -65,18 +65,18 @@ void DUOStereoDriver::fillDUOImages(sensor_msgs::Image& leftImage, sensor_msgs::
 	// size_t = step * height    , where height is the amount of columns in pixels
 	//
 	sensor_msgs::fillImage(	leftImage, 								// image reference
-							sensor_msgs::image_encodings::MONO8, 	// type of encoding
-							pFrameData->height, 					// columns in pixels 
-							pFrameData->width,						// rows in pixels
-							pFrameData->width,						// step size 
-							pFrameData->leftData);					// left camera data pointer
+			sensor_msgs::image_encodings::MONO8, 	// type of encoding
+			pFrameData->height, 					// columns in pixels
+			pFrameData->width,						// rows in pixels
+			pFrameData->width,						// step size
+			pFrameData->leftData);					// left camera data pointer
 
-    sensor_msgs::fillImage( rightImage,
-			                sensor_msgs::image_encodings::MONO8,
-			                pFrameData->height,
-			                pFrameData->width,
-			                pFrameData->width,
-			                pFrameData->rightData);
+	sensor_msgs::fillImage( rightImage,
+			sensor_msgs::image_encodings::MONO8,
+			pFrameData->height,
+			pFrameData->width,
+			pFrameData->width,
+			pFrameData->rightData);
 }
 
 
@@ -89,44 +89,44 @@ bool DUOStereoDriver::validateCameraInfo(const sensor_msgs::Image &image, const 
 void DUOStereoDriver::publishImages(const sensor_msgs::ImagePtr image[TWO_CAMERAS])
 {
 
-    for (int i = 0; i < TWO_CAMERAS; i++)
-    {
-        // Get current CameraInfo data and populate ImagePtr Array
-        sensor_msgs::CameraInfoPtr
-          		ci(new sensor_msgs::CameraInfo(_cinfo[i]->getCameraInfo()));
+	for (int i = 0; i < TWO_CAMERAS; i++)
+	{
+		// Get current CameraInfo data and populate ImagePtr Array
+		sensor_msgs::CameraInfoPtr
+		ci(new sensor_msgs::CameraInfo(_cinfo[i]->getCameraInfo()));
 
-        // If camera info and image width and height dont match
-        // then set calibration_matches to false so we then 
-        // know if we should reset the camera info or not
-        if (!validateCameraInfo(*image[i], *ci)) 
-        {
-        	if(_calibrationMatches[i])
-        	{
-        		_calibrationMatches[i] = false; 
-        		ROS_WARN_STREAM("*" << _camera_name << "/" << CameraNames[i] << "* "
-        							<< "camera_info is different then calibration info. "
-        							<< "Uncalibrated image being sent.");
-        	}
-        	ci.reset(new sensor_msgs::CameraInfo());
-            ci->height 	= image[i]->height;
-            ci->width 	= image[i]->width;
-        }
-        else if (!_calibrationMatches[i])
-        {
-        	// Calibration is now okay
-        	_calibrationMatches[i] = true; 
-    		ROS_WARN_STREAM("*" << _camera_name << "/" << CameraNames[i] << "* "
-    							<< "is now publishing calibrated images. ");
-        }
+		// If camera info and image width and height dont match
+		// then set calibration_matches to false so we then
+		// know if we should reset the camera info or not
+		if (!validateCameraInfo(*image[i], *ci))
+		{
+			if(_calibrationMatches[i])
+			{
+				_calibrationMatches[i] = false;
+				ROS_WARN_STREAM("*" << _camera_name << "/" << CameraNames[i] << "* "
+						<< "camera_info is different then calibration info. "
+						<< "Uncalibrated image being sent.");
+			}
+			ci.reset(new sensor_msgs::CameraInfo());
+			ci->height 	= image[i]->height;
+			ci->width 	= image[i]->width;
+		}
+		else if (!_calibrationMatches[i])
+		{
+			// Calibration is now okay
+			_calibrationMatches[i] = true;
+			ROS_WARN_STREAM("*" << _camera_name << "/" << CameraNames[i] << "* "
+					<< "is now publishing calibrated images. ");
+		}
 
-        ci->header.frame_id = image[i]->header.frame_id;
-        ci->header.stamp 	= image[i]->header.stamp;
+		ci->header.frame_id = image[i]->header.frame_id;
+		ci->header.stamp 	= image[i]->header.stamp;
 
-        _imagePub[i].publish(image[i], ci);
-    }
+		_imagePub[i].publish(image[i], ci);
+	}
 
-    sensor_msgs::clearImage(*image[0]);
-    sensor_msgs::clearImage(*image[1]);
+	sensor_msgs::clearImage(*image[0]);
+	sensor_msgs::clearImage(*image[1]);
 }
 
 
@@ -141,85 +141,85 @@ void CALLBACK DUOCallback(const PDUOFrame pFrameData, void *pUserData)
 	sensor_msgs::ImagePtr 	image[duoDriver.TWO_CAMERAS];	
 
 	// Initialize array of image pointers
-    for (int i = 0; i < duoDriver.TWO_CAMERAS; i++)
-    {
-    	image[i] = sensor_msgs::ImagePtr(new sensor_msgs::Image);    	
-    }
+	for (int i = 0; i < duoDriver.TWO_CAMERAS; i++)
+	{
+		image[i] = sensor_msgs::ImagePtr(new sensor_msgs::Image);
+	}
 
-    // Dereferencing individual images to fill with pFrameData from camera
-    // Then publish the images
-    //duoDriver.fillDUOImages(*image[duoDriver.LEFT_CAM], *image[duoDriver.RIGHT_CAM], pFrameData);
-    duoDriver.fillDUOImages(*image[1], *image[0], pFrameData); //we had to swich the image 
+	// Dereferencing individual images to fill with pFrameData from camera
+	// Then publish the images
+	//duoDriver.fillDUOImages(*image[duoDriver.LEFT_CAM], *image[duoDriver.RIGHT_CAM], pFrameData);
+	duoDriver.fillDUOImages(*image[1], *image[0], pFrameData); //we had to swich the image
 
 
-    /*--------------------------------------------------------*/
-    // Imu stuff
-    sensor_msgs::Imu img_msg;
-    img_msg.angular_velocity.x = pFrameData->gyroData[0];
-    img_msg.angular_velocity.y = pFrameData->gyroData[1];
-    img_msg.angular_velocity.z = pFrameData->gyroData[2];
+	/*--------------------------------------------------------*/
+	// Imu stuff
+	sensor_msgs::Imu img_msg;
+	img_msg.angular_velocity.x = pFrameData->gyroData[0];
+	img_msg.angular_velocity.y = pFrameData->gyroData[1];
+	img_msg.angular_velocity.z = pFrameData->gyroData[2];
 
-    img_msg.linear_acceleration.x = pFrameData->accelData[0];
-    img_msg.linear_acceleration.y = pFrameData->accelData[1];
-    img_msg.linear_acceleration.z = pFrameData->accelData[2];
+	img_msg.linear_acceleration.x = pFrameData->accelData[0];
+	img_msg.linear_acceleration.y = pFrameData->accelData[1];
+	img_msg.linear_acceleration.z = pFrameData->accelData[2];
 
-    img_msg.header.stamp = ros::Time( double(pFrameData->timeStamp) * 1.e-4);
+	img_msg.header.stamp = ros::Time( double(pFrameData->timeStamp) * 1.e-4);
 
-    duo3d_ros::Duo3d combined_msg;
-    combined_msg.header = img_msg.header;
-    combined_msg.imu = img_msg;
-    combined_msg.left_image = *image[0];
-    combined_msg.right_image = *image[1];
+	duo3d_ros::Duo3d combined_msg;
+	combined_msg.header = img_msg.header;
+	combined_msg.imu = img_msg;
+	combined_msg.left_image = *image[0];
+	combined_msg.right_image = *image[1];
 
-    duoDriver.publishCombinedData(combined_msg);
-    duoDriver.publishImages(image);
+	duoDriver.publishCombinedData(combined_msg);
+	duoDriver.publishImages(image);
 
-    sensor_msgs::MagneticField mag_msg;
-    mag_msg.magnetic_field.x = pFrameData->magData[0];
-    mag_msg.magnetic_field.y = pFrameData->magData[1];
-    mag_msg.magnetic_field.z = pFrameData->magData[2];
+	sensor_msgs::MagneticField mag_msg;
+	mag_msg.magnetic_field.x = pFrameData->magData[0];
+	mag_msg.magnetic_field.y = pFrameData->magData[1];
+	mag_msg.magnetic_field.z = pFrameData->magData[2];
 
-    mag_msg.header.stamp = ros::Time(double(pFrameData->timeStamp) * 1.e-4);
+	mag_msg.header.stamp = ros::Time(double(pFrameData->timeStamp) * 1.e-4);
 
-    sensor_msgs::Temperature temp_msg;
-    temp_msg.temperature = pFrameData->tempData;
-    temp_msg.header.stamp = ros::Time(double(pFrameData->timeStamp) * 1.e-4);
+	sensor_msgs::Temperature temp_msg;
+	temp_msg.temperature = pFrameData->tempData;
+	temp_msg.header.stamp = ros::Time(double(pFrameData->timeStamp) * 1.e-4);
 
-    duoDriver.publishImuData(img_msg);
-    duoDriver.publishMagData(mag_msg);
-    duoDriver.publishTempData(temp_msg);
+	duoDriver.publishImuData(img_msg);
+	duoDriver.publishMagData(mag_msg);
+	duoDriver.publishTempData(temp_msg);
 }
 
 void DUOStereoDriver::publishMagData(const sensor_msgs::MagneticField &mag_msg)
 {
-    _mag_pub.publish(mag_msg);
+	_mag_pub.publish(mag_msg);
 }
 
 void DUOStereoDriver::publishCombinedData(const duo3d_ros::Duo3d &combined_msg)
 {
-    _combined_pub.publish(combined_msg);
-    if (_combined_pub.getNumSubscribers())
-    {
-    	msgs_in_queue++;
-    	if (!has_subscriber)
-    		ROS_INFO("DUO3d found subscriber");
-    	has_subscriber = true;
-    } else {
-    	msgs_in_queue = 0;
-    	if (has_subscriber)
-    		ROS_WARN("DUO3d has lost the subscriber");
-    	has_subscriber = false;
-    }
+	_combined_pub.publish(combined_msg);
+	if (_combined_pub.getNumSubscribers())
+	{
+		msgs_in_queue++;
+		if (!has_subscriber)
+			ROS_INFO("DUO3d found subscriber");
+		has_subscriber = true;
+	} else {
+		msgs_in_queue = 0;
+		if (has_subscriber)
+			ROS_WARN("DUO3d has lost the subscriber");
+		has_subscriber = false;
+	}
 }
 
 void DUOStereoDriver::publishImuData(const sensor_msgs::Imu &img_msg)
 {
-     _pub.publish(img_msg);
+	_pub.publish(img_msg);
 }
 
 void DUOStereoDriver::publishTempData(const sensor_msgs::Temperature &temp_msg)
 {
-     _temp_pub.publish(temp_msg);
+	_temp_pub.publish(temp_msg);
 }
 
 bool DUOStereoDriver::initializeDUO()
@@ -287,14 +287,14 @@ bool DUOStereoDriver::initializeDUO()
 
 
 	// Find optimal binning parameters for given (width, height)
-    // This maximizes sensor imaging area for given resolution
-    int binning = DUO_BIN_NONE;
-    if(resWidth <= 752/2) 
-        binning += DUO_BIN_HORIZONTAL2;
-    if(resHeight <= 480/4) 
-        binning += DUO_BIN_VERTICAL4;
-    else if(resHeight <= 480/2) 
-        binning += DUO_BIN_VERTICAL2;
+	// This maximizes sensor imaging area for given resolution
+	int binning = DUO_BIN_NONE;
+	if(resWidth <= 752/2)
+		binning += DUO_BIN_HORIZONTAL2;
+	if(resHeight <= 480/4)
+		binning += DUO_BIN_VERTICAL4;
+	else if(resHeight <= 480/2)
+		binning += DUO_BIN_VERTICAL2;
 
 	/*
 	 * @brief
@@ -343,7 +343,7 @@ bool DUOStereoDriver::initializeDUO()
 	if(EnumerateResolutions(&_duoResolutionInfo, 1, resWidth, resHeight, binning, framesPerSecond))
 	{
 		ROS_INFO("Resolution Parameters Check: PASSED");
-		
+
 		if(OpenDUO(&_duoInstance))
 		{
 			GetDUODeviceName(		_duoInstance, _duoDeviceName);
@@ -389,48 +389,50 @@ bool DUOStereoDriver::initializeDUO()
 // based on the dynamic_reconfigure parameters it recieves.
 void DUOStereoDriver::dynamicCallback(duo3d_ros::DuoConfig &config, uint32_t level) 
 {
-  	//ROS_INFO("Reconfigure Request: %f %f %f", 
-    //        	config.exposure, config.gain, 
-    //        	config.LED);
+	//ROS_INFO("Reconfigure Request: %f %f %f",
+	//        	config.exposure, config.gain,
+	//        	config.LED);
 
-  	// if any parameters have changed, then let the DUO camera know and change them
-  	if(_duoExposure != config.exposure)
-  	{
-  		_duoExposure = config.exposure;
-    	SetDUOExposure(_duoInstance, _duoExposure);
-  	}
-  	if(_duoGain != config.gain)
-  	{
-  		_duoGain = config.gain;
-    	SetDUOGain(_duoInstance, _duoGain);
-  	}
-  	if(_duoLEDLevel != config.LED)
-  	{
-  		_duoLEDLevel = config.LED;
-    		SetDUOLedPWM(_duoInstance, _duoLEDLevel);
+	// if any parameters have changed, then let the DUO camera know and change them
+	if(_duoExposure != config.exposure)
+	{
+		_duoExposure = config.exposure;
+		SetDUOExposure(_duoInstance, _duoExposure);
+	}
+	if(_duoGain != config.gain)
+	{
+		_duoGain = config.gain;
+		SetDUOGain(_duoInstance, _duoGain);
+	}
+	if(_duoLEDLevel != config.LED)
+	{
+		_duoLEDLevel = config.LED;
+		SetDUOLedPWM(_duoInstance, _duoLEDLevel);
 		SetDUOLedPWM(_duoInstance, 0);	//TODO HACK HACK
-  	}
-  	if(_duoCameraSwap != config.CameraSwap)
-  	{
-  		_duoCameraSwap = config.CameraSwap;
-  		SetDUOCameraSwap(_duoInstance, _duoCameraSwap);
-  	}
-  	if(_duoHorizontalFlip != config.HorizontalFlip)
-  	{
-  		_duoHorizontalFlip = config.HorizontalFlip;
-  		SetDUOHFlip(_duoInstance, _duoHorizontalFlip);
-  	}
-  	if(_duoVerticalFlip != config.VerticalFlip)
-  	{
-  		_duoVerticalFlip = config.VerticalFlip;
-  		SetDUOVFlip(_duoInstance, _duoVerticalFlip);
-  	}
+	}
+	if(_duoCameraSwap != config.CameraSwap)
+	{
+		_duoCameraSwap = config.CameraSwap;
+		SetDUOCameraSwap(_duoInstance, _duoCameraSwap);
+	}
+	if(_duoHorizontalFlip != config.HorizontalFlip)
+	{
+		_duoHorizontalFlip = config.HorizontalFlip;
+		SetDUOHFlip(_duoInstance, _duoHorizontalFlip);
+	}
+	if(_duoVerticalFlip != config.VerticalFlip)
+	{
+		_duoVerticalFlip = config.VerticalFlip;
+		SetDUOVFlip(_duoInstance, _duoVerticalFlip);
+	}
 
 }
 
 void DUOStereoDriver::msgProcessedCb(const std_msgs::Int32 &msg)
 {
 	msgs_in_queue--;
+	if (msgs_in_queue > 10)
+		ROS_WARN("DUO3d queue very long! %d messages, queue size is %d", msgs_in_queue, msg.data);
 	if (msgs_in_queue > msg.data)
 		ROS_WARN("DUO3d queue overflow! Sent %d messages, queue size is %d", msgs_in_queue, msg.data);
 }
